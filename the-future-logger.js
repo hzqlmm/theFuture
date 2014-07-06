9f06ea9ee5afbd986c07d7d8ec89d290446f0730
hzqlmm <hzqlmm@gmail.com>
2014-07-06 23:32:49 +0800

    enhance for one log each day

--- the-future-logger.js
+++ the-future-logger.js
@@ -1,17 +1,55 @@
+require('./date-extend.js');
 var bunyan = require('bunyan');
+var server_options = {};
+var db_options = {
+    w: -1
+};
 
-var logger = bunyan.createLogger({
-    name: 'theFuture',
-    streams: [{
-        level: 'info',
-        path: './log/ripple-info' + new Date().toJSON() + '.js'
-    }, {
-        level: 'error',
-        path: './log/ripple-error' + new Date().toJSON() + '.js' // log ERROR and above to a file
-    }]
-});
+var mongodb = require("mongodb");
+var mongoserver = new mongodb.Server('localhost', 27017, server_options);
+var db = new mongodb.Db('test', mongoserver, db_options);
 
-function Logger() {};
+Logger.prototype.getNextSequence = function(name) {
+    var ret = db.collection('counters').findAndModify({
+        query: {
+            _id: name
+        },
+        update: {
+            $inc: {
+                seq: 1
+            }
+        },
+        new: true
+    }, function(err, result) {
+        db.close();
+    });
+
+    return ret.seq;
+}
+
+function Logger() {
+    this.logger;
+    this.getNewLog();
+    setInternal(this.getNewLog, 86400000); //we will get new log each day.
+};
+
+Logger.prototype.getNewLog = function() {
+    var self = this;
+
+    var logId = getNextSequence('logId');
+    var date = new Date().format('MM-dd-yyyy');
+
+    self.logger = bunyan.createLogger({
+        name: 'theFuture',
+        streams: [{
+            level: 'info',
+            path: './log/ripple-info-' + logId + date + '.js'
+        }, {
+            level: 'error',
+            path: './log/ripple-error' + logId + date + '.js' // log ERROR and above to a file
+        }]
+    });
+}
 
 Logger.prototype.log = function() {
     var self = this;
@@ -20,11 +58,16 @@ Logger.prototype.log = function() {
         return;
     }
     if (arguments[0]) { //check if we want to log something, this value is boolean type.
-        for (var i = 1; i < arguNum; i = i + 1) {
-            logger.info(arguments[i]);
-        }
+        delete arguments[0];
+        logger.info(arguments);
     }
 };
+
+Logger.prototype.getLogId = function() {
+
+}
+
+
 
 process.on('uncaughtException', function(err) {
     // prevent infinite recursion
